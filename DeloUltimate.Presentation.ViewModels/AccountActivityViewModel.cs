0664dae70a21248e1aa4b43fb2d49134cdabed09
Михаил Джурмий <misha.dzhurmiy@gmail.com>
2022-05-19 13:36:59 +0800

    Add enumerable Modify AccountActivityPage and CabinetAttachmentPage Modify AccountActivityViewModel and CabinetAttachmentViewModel

--- DeloUltimate.Presentation.ViewModels/AccountActivityViewModel.cs
+++ DeloUltimate.Presentation.ViewModels/AccountActivityViewModel.cs
@@ -1,18 +1,33 @@
 ﻿using DeloUltimate.Domain.Entities;
 using DeloUltimate.Presentation.ViewModels.Abstract;
+using DeloUltimate.Presentation.ViewModels.Commands;
 using DeloUltimate.Services.Interfaces;
 using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows.Input;
 
 namespace DeloUltimate.Presentation.ViewModels
 {
     public class AccountActivityViewModel : ViewModel
     {
-        private IDataImportService<AccountActivity> _service;
+        private IDataImportService<AccountActivity> _dataImportService;
+        private IDataExportService<AccountActivity> _dataExportService;
 
-        public AccountActivityViewModel(IDataImportService<AccountActivity> service)
+        public AccountActivityViewModel(IDataImportService<AccountActivity> dataImportService,
+                                        IDataExportService<AccountActivity> dataExportService)
         {
-            _service = service;
-            AccountActivityCollection = new ObservableCollection<AccountActivity>(service.ImportFromDatabase());
+            _dataImportService = dataImportService;
+            _dataExportService = dataExportService;
+
+            #region Registration
+
+            #region Commands
+
+            FetchData = new RelayCommand(OnFetchDataExecuted, CanFetchDataExecute);
+
+            #endregion
+
+            #endregion
         }
 
         #region Properties
@@ -25,6 +40,23 @@ namespace DeloUltimate.Presentation.ViewModels
         {
             get => _accountActivityCollection;
             set => Set(ref _accountActivityCollection, value);
+        }
+
+        #endregion
+
+        #endregion
+
+        #region Commands
+
+        #region FetchData
+
+        public ICommand FetchData { get; private set; }
+
+        private bool CanFetchDataExecute(object p) => AccountActivityCollection is null;
+
+        private void OnFetchDataExecuted(object p)
+        {
+            AccountActivityCollection = new ObservableCollection<AccountActivity>(_dataImportService.ImportFromDatabase());
         }
 
         #endregion
--- DeloUltimate.Presentation.ViewModels/CabinetAttachmentViewModel.cs
+++ DeloUltimate.Presentation.ViewModels/CabinetAttachmentViewModel.cs
@@ -1,30 +1,139 @@
 ﻿using DeloUltimate.Domain.Entities;
 using DeloUltimate.Presentation.ViewModels.Abstract;
+using DeloUltimate.Presentation.ViewModels.Commands;
+using DeloUltimate.Presentation.ViewModels.Models;
 using DeloUltimate.Services.Interfaces;
+using System;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Input;
 
 namespace DeloUltimate.Presentation.ViewModels
 {
     public class CabinetAttachmentViewModel : ViewModel
     {
-        public CabinetAttachmentViewModel(IDataImportService<Cabinet> service)
+        private IDataImportService<Cabinet> _dataImportService;
+
+        public CabinetAttachmentViewModel(IDataImportService<Cabinet> dataImportService)
         {
-            _cabinetCollection = new ObservableCollection<Cabinet>(service.ImportFromDatabase());
+            _dataImportService = dataImportService;
+
+            #region Registration
+
+            #region Commands
+
+            FetchData = new RelayCommand(OnFetchDataExecuted, CanFetchDataExecute);
+
+            #endregion
+
+            #endregion
         }
 
         #region Properties
 
+        #region IsDataFetched
+
+        private bool _isDataFetched = false;
+
+        public bool IsDataFetched
+        {
+            get => _isDataFetched;
+            set => Set(ref _isDataFetched, value);
+        }
+
+        #endregion
+
         #region CabinetCollection
 
-        private ObservableCollection<Cabinet> _cabinetCollection;
+        private ObservableCollection<Cabinet> _cabinetCollection = new ObservableCollection<Cabinet>();
 
-        public ObservableCollection<Cabinet> CabinetCollection
+        private ObservableCollection<Cabinet> CabinetCollection
         {
             get => _cabinetCollection;
             set => Set(ref _cabinetCollection, value);
         }
 
         #endregion
+
+        #region CabinetCollectionView
+
+        private ListCollectionView _cabinetCollectionView;
+
+        public ListCollectionView CabinetCollectionView
+        {
+            get => _cabinetCollectionView;
+            set => Set(ref _cabinetCollectionView, value);
+        }
+
+        #endregion
+
+        #region CabinetFilterOptions
+
+        private CabinetFilterOptions _cabinetFilterOptions = CabinetFilterOptions.EmployeeName;
+
+        public CabinetFilterOptions CabinetFilterOptions
+        {
+            get => _cabinetFilterOptions;
+            set => Set(ref _cabinetFilterOptions, value);
+        }
+
+        #endregion
+
+        #region FilterText
+
+        private string _filterText = string.Empty;
+
+        public string FilterText
+        {
+            get => _filterText;
+            set
+            {
+                Set(ref _filterText, value);
+                CabinetCollectionView.Filter += CabinetFilter;
+            }
+        }
+
+        #endregion
+
+        #endregion
+
+        #region Commands
+
+        #region FetchData
+        public ICommand FetchData { get; private set; }
+
+        private bool CanFetchDataExecute(object p) => IsDataFetched == false;
+
+        private void OnFetchDataExecuted(object p)
+        {
+            CabinetCollection = new ObservableCollection<Cabinet>(_dataImportService.ImportFromDatabase());
+            CabinetCollectionView = new ListCollectionView(CabinetCollection);
+            IsDataFetched = true;
+        }
+
+        #endregion
+
+        #endregion
+
+        #region EventHandlers
+
+        private bool CabinetFilter(object obj)
+        {
+            Cabinet cabinet = obj as Cabinet;
+            switch (CabinetFilterOptions)
+            {
+                case (CabinetFilterOptions.EmployeeName):
+                    return string.IsNullOrEmpty(FilterText) ? true :
+                        cabinet.EmployeeName.ToLower().Contains(FilterText.ToLower());
+                case (CabinetFilterOptions.DepartmentName):
+                    return string.IsNullOrEmpty(FilterText) ? true :
+                        cabinet.CabinetName.ToLower().Contains(FilterText.ToLower());
+                default: return false;
+            }
+        }
 
         #endregion
     }
