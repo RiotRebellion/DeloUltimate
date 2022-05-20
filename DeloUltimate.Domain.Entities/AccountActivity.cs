992521fac46509bebef3d3a9562fe22053674807
Михаил Джурмий <misha.dzhurmiy@gmail.com>
2022-05-20 15:18:37 +0800

    Add attribute to AccountActivity

--- DeloUltimate.Domain.Entities/AccountActivity.cs
+++ DeloUltimate.Domain.Entities/AccountActivity.cs
@@ -1,14 +1,19 @@
-﻿using DeloUltimate.Domain.Entities.Base;
+﻿using DeloUltimate.Domain.Entities.Attributes;
+using DeloUltimate.Domain.Entities.Base;
 using System;
 
 namespace DeloUltimate.Domain.Entities
 {
+    [RussianName("Активность пользователей")]
     public class AccountActivity : IEntity
     {
+        [RussianName("Имя пользователя")]
         public string AccountName { get; set; }
 
+        [RussianName("Дата последней авторизации")]
         public DateTime LastAutorizationDate { get; set; }
 
+        [RussianName("Прошло дней")]
         public int DaysGone { get; set; }
     }
 }
