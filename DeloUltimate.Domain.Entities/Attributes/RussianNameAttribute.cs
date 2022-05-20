a6930031966d252502eff58627173e775c7981f5
Михаил Джурмий <misha.dzhurmiy@gmail.com>
2022-05-20 15:18:22 +0800

    Add RussianNameAttribute

---
+++ DeloUltimate.Domain.Entities/Attributes/RussianNameAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace DeloUltimate.Domain.Entities.Attributes
+{
+    public class RussianNameAttribute : Attribute
+    {
+        public string Name;
+
+        public RussianNameAttribute() { }
+
+        public RussianNameAttribute(string name)
+        {
+            Name = name;
+        }
+    }
+}
